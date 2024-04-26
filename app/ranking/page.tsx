74e04fdcf3d1932192b0d34b2e3f0426624846be
karolhas <karolekhas@gmail.com>
2024-04-26 15:12:54 +0200

    Feat: deleted backend features

--- app/ranking/page.tsx
+++ app/ranking/page.tsx
@@ -2,44 +2,12 @@
 
 import { Button } from "@/components/Button";
 import Container from "@/components/Container";
-import { useEffect, useState } from "react";
-
-interface Score {
-  _id: string;
-  nickname: string;
-  score: number;
-}
 
 const Ranking: React.FC = () => {
-  const [scores, setScores] = useState<Score[]>([]);
-
-  useEffect(() => {
-    const fetchScores = async () => {
-      try {
-        const response = await fetch("/api/getScores");
-        const data = await response.json();
-
-        if (Array.isArray(data) && data.length > 0) {
-          setScores(data as Score[]);
-        }
-      } catch (error) {
-        console.error("Error fetching scores:", error);
-      }
-    };
-
-    fetchScores();
-  }, []);
-
   return (
     <Container>
       <h1>RANKING</h1>
-      <ul>
-        {scores.map((score) => (
-          <li key={score._id}>
-            {score.nickname} - {score.score}
-          </li>
-        ))}
-      </ul>
+      <h2>Wkrótce...</h2>
       <Button
         variant="outline"
         size="sm"
